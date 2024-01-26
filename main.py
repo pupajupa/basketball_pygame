2c7d9bce85fa9ac934483e7717e2e2671cf4bec1
pupajupa <garilarulit@gmail.com>
2024-01-26 03:58:22 +0300

    ADD_BOUNCING

--- main.py
+++ main.py
@@ -2,15 +2,44 @@ import pygame
 
 pygame.init()
 
-#global variable 
-width = 750
-heigth = 550
-screen = pygame.display.set_mode((width,heigth))
-gameOn = True
+# global variable
+width = 1000
+heigth = 680
+screen = pygame.display.set_mode((width, heigth))
 
-while gameOn:
-    #taking event
-    for event in pygame.event.get():
-        print(event)
-        if event.type == pygame.QUIT:
-            gameOn=False
+background = pygame.image.load("assets/images/background.png")
+base = pygame.image.load("assets/images/road.png")
+basketball = pygame.image.load("assets/images/basketball.png")
+
+
+def main():
+    gameOn = True
+    baseX = 0
+    baseY = heigth - 120
+    ballX = 100
+    ballY = baseY - 120
+    gravity = 10
+    bouncing = 25
+
+    while gameOn:
+        # taking event
+        for event in pygame.event.get():
+            # print(event)
+            if event.type == pygame.QUIT:
+                gameOn = False
+        screen.blit(background, (0, 0))
+        screen.blit(base, (baseX, baseY))
+        screen.blit(basketball, (ballX, ballY))
+
+        # bouncing
+        ballY -= bouncing
+        bouncing -= 1
+        ballY += gravity
+        if ballY > baseY - 20:
+            bouncing = 25
+
+        pygame.display.update()
+
+
+if __name__ == "__main__":
+    main()
